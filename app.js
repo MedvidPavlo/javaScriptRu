067046fa25adf8ebab14e60600cce016780969f4
PavloMedvid <dorotei.post@gmail.com>
2022-07-22 17:46:18 +0300

    First commit 20220722

---
+++ app.js
@@ -0,0 +1,234 @@
+// let year = prompt('В каком году была опубликована спецификация ECMAScript-2015?', '');
+// if (year == 2015) alert('Ok');
+
+// let year = 2015;
+// if (year == 2015) {
+//     console.log('Yes');
+//     console.log('yes', ', ', 'yes');
+// } else {
+//     console.log('No');
+// }
+
+// let accessAllowed;
+// let age = prompt('How old are you?', '');
+// if (age > 18) {
+//     accessAllowed = true;
+// } else {
+//     accessAllowed = false;
+// }
+// alert(accessAllowed);
+    
+// let js = prompt('Какое "официальное" название JavaScript?', '');
+// if (js === 'ECMAScript') {
+//     alert('Верно!');
+// } else {
+//     alert('Не знаете? “ECMAScript”!');
+// }
+
+// let number = prompt('Number?', '')
+// if (number > 0) {
+//     alert(1);
+// } else if (number < 0) {
+//     alert(-1);
+// } else {
+//     alert(0);
+// }
+
+// let a = 2; let b = 10;
+// let result=a+b;
+// if (a + b < 4) {
+//     result='Мало';
+//     console.log(result);
+// } else {
+//     result='Много';
+//     console.log(result);
+// }
+
+// let a = prompt('', ''); let b = prompt('', '');
+// let result = (a + b < 4) ? 'Мало' :
+//     'Много';
+// alert(result);
+
+// let message;
+// if (login == 'Сотрудник') {
+//   message = 'Привет';
+// } else if (login == 'Директор') {
+//   message = 'Здравствуйте';
+// } else if (login == '') {
+//   message = 'Нет логина';
+// } else {
+//   message = '';
+// }
+
+// let login = prompt('', '');
+// let message = (login == 'Сотрудник') ? 'Привет' :
+//     (login == 'Директор') ? 'Здравствуйте' :
+//         (login == '') ? 'Нет логина' :
+//             '';
+// alert(message);
+    
+//   function showMessage() {
+//   alert( 'Всем привет!' );
+// }
+// showMessage();
+
+// let userName = 'pavlo';
+// function showMessage() {
+//     let message = 'Hello, ' + userName;
+//     alert(message);
+// }
+// showMessage();
+
+// let userName = 'Pavlo';
+// function showMessage(){
+//     userName = 'Ivan';
+//     let message = 'Hello, ' + userName;
+// alert(message);
+// }
+// alert(userName);
+// showMessage();
+// alert(userName);
+
+// function step() {
+//     let a = prompt('', '');
+//     let b = prompt('', '');
+//     let sum = a + b;
+//     alert(sum);
+// }
+// step();
+
+// function showMessage(from, text) {
+//     alert(from + ': ' + text);
+// }
+// showMessage('anna', 'Hello!');
+// showMessage('anna', 'How are you?');
+
+// function имя(параметры) {
+//   ...тело...
+// }
+// function sum(a, b) {
+//     let result = a + b;
+//     alert(result);
+// }
+// sum(2, 3);
+
+// function sum(a, b) {
+//   return a + b;
+// }
+// let result = sum(1, 2);
+// alert( result ); // 3
+
+// function checkAge(age) {
+//     if (age > 18) {
+//         return true;
+//     } else {
+//         return confirm('А родители разрешили?');
+//     }
+// }
+// let age = prompt('Сколько вам лет?', 18);
+// if (checkAge(age)) {
+//     alert('Доступ получен');
+// } else {
+//     alert('Доступ закрыт');
+// }
+
+// function doNothing() { }
+// alert(doNothing()===undefined);
+// function doNothing() { return; }
+// alert(doNothing()===undefined);
+
+// function getString(){
+//     const greeting = 'Hello, Mate Academy!';
+//     alert(greeting);
+//     return greeting;
+    
+    
+// }
+// getString();
+
+// let name='Pavlo'
+// function greeter(name) {
+//   alert( `Hi, ${name}!`);
+// }
+// greeter('Ivan');
+
+// let name='pavlo'; let partOfTheDay='night';
+// function greeter(name, partOfTheDay) {
+//   alert(`Good ${partOfTheDay}, ${name}!`);
+// }
+// greeter(name, partOfTheDay);
+
+// function double(num){
+//   alert(num*2);
+// }
+// double(5);
+
+// let i = 0;
+// while (i < 3) {
+//     console.log(i);
+//     i++;
+// }
+
+// let i = 0;
+// do {
+//     alert(i);
+//     i++;
+// } while (i < 3);
+
+// for (let i = 0; i < 3; i++){
+//     alert(i);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
